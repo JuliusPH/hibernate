7cd5925f3ebe885bb46ed97306e060be31f2a698
JuliusPH <juliuscessarlapugot@gmail.com>
2016-05-24 14:06:50 +0800

    Add sql files 20160524

---
+++ hibernate-xml/infra/buildtools/sql/20160524_01.sql
@@ -0,0 +1,16 @@
+INSERT INTO address VALUES (default, '5', 'Maysan', 'Valenzuela City', '1440');
+
+INSERT INTO person VALUES (default, 'John Paul', 'Canceran', 'Lapugot', 4, '1997-08-28', 75, null, false, 'Male');
+
+INSERT INTO contact VALUES (default, 'Landline', '5215112', 4);
+
+DELETE FROM person WHERE id = 4;
+DELETE FROM address WHERE id = 4;
+
+
+
+INSERT INTO address VALUES (default, '27', 'Gen. T', 'Valenzuela City', '1440');
+
+INSERT INTO person VALUES (default, 'Alex', 'Dela Cruz', 'Garzon', 5, '1996-05-14', 85, null, false, 'Male');
+
+INSERT INTO contact VALUES (default, 'Landline', '8214712', 5);
